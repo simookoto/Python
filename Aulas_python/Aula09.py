5150d0d65cfc02c1a079af4ec777b9bced8f2864
simookoto <simohayha14072011@gmail.com>
2026-03-27 13:34:07 -0300

    update Aulas_python/Aula09.py

--- Aulas_python/Aula09.py
+++ Aulas_python/Aula09.py
@@ -1,12 +1,3 @@
 frase='Curso em Video Python'
-#frase[9]#
-#frase[9:14]#
-#frase[9:21:2]#
-#frase[:5]#
-#vai começar do inicio ate o caracter 5#
-#frase[15:]#
-#vai começar do caracter 15 ate o final#
-len(frase)
-frase.count('o',0,13)
-frase.find('deo')
-'video' in frase
+dividido=frase.split()
+print(dividido[2][3])
