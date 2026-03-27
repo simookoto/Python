495fdc736890cd390cf87ec068395afcc37a2afe
simookoto <simohayha14072011@gmail.com>
2026-03-26 22:16:23 -0300

    create Exercícios/Exercício18.py

---
+++ Exercícios/Exercício18.py
@@ -0,0 +1,8 @@
+import math
+an=float(input('digite um angulo:'))
+se=math.sin(math.radians(an))
+print('o angulo é {} e o seno é {:.2f}' .format(an, se, ))
+co=math.cos(math.radians(an))
+print('o angulo é {} e o coseno é {:.2f}' .format(an, co))
+ta=math.tan(math.radians(an))
+print('o angulo é {} e o tangente é {:.2f}' .format(an, ta))
