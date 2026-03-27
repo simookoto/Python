9e9136435e3379646fda47ce73f87aff2f4bdfc4
simookoto <simohayha14072011@gmail.com>
2026-03-26 22:09:11 -0300

    create Aulas_python/Aula07a.py

---
+++ Aulas_python/Aula07a.py
@@ -0,0 +1,9 @@
+n1=int(input('um valor:'))
+n2=int(input('outro valor:'))
+s=n1+n2
+m=n1*n2
+d=n1/n2
+di=n1//n2
+e=n1**n2
+print('a soma é {} o produto é {} e a divisão é {:.3}'.format(s, m, d))
+print('divisão inteira é {} e potencia é {}'.format(di, e))
