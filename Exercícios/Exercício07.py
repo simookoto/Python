12a7bc079214e87aef1a462bb2e4de8e39230649
simookoto <simohayha14072011@gmail.com>
2026-03-26 22:11:45 -0300

    create Exercícios/Exercício07.py

---
+++ Exercícios/Exercício07.py
@@ -0,0 +1,4 @@
+n1=float(input('digite sua primeira nota:'))
+n2=float(input('digite sua segunda nota:'))
+m=(n1+n2)/2
+print('sua primeira nota é {}, sua segunda nota é {}, e sua media é {}' .format(n1, n2, m))
