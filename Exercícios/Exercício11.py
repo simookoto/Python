ce1d641d9fb11fd928d3986dcb0c4413c6034bd0
simookoto <simohayha14072011@gmail.com>
2026-03-26 22:13:42 -0300

    create Exercícios/Exercício11.py

---
+++ Exercícios/Exercício11.py
@@ -0,0 +1,6 @@
+lp=float(input('qual a largura da sua parede:'))
+ap=float(input('qual a altura da sua parede:'))
+m2=lp*ap
+t=m2/2
+print('você tem {}x{} e {}m² de parede' .format(lp, ap, m2))
+print('você vai precisar de {}L de tinta pra pintar' .format(t))
