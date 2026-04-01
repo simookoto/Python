79d5edf21c494744d2bc87e8be14805b7d917f95
simookoto <simohayha14072011@gmail.com>
2026-04-01 19:12:24 -0300

    update Exercícios/Exercício22.py

---
+++ Exercícios/Exercício22.py
@@ -0,0 +1,6 @@
+nome=str(input('qual seu nome completo?')).strip()
+print('analisando seu nome...')
+print('seu nome em maiúsculo é {}' .format(nome.upper()))
+print('seu nome em minúsculas é {}' .format(nome.lower()))
+print('seu nome ao total tem {} letras' .format(len(nome)-nome.count(' ')))
+print('seu primeiro nome tem {} letras' .format(nome.find(' ')))
