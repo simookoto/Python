4ef89a3af65329e517578181ab500508481a2cbb
simookoto <simohayha14072011@gmail.com>
2026-04-16 16:12:36 -0300

    update Exercícios/Exercício26.py

---
+++ Exercícios/Exercício26.py
@@ -0,0 +1,4 @@
+frase=str(input('digite uma frase:')).upper()
+print('a letra A aparece {} vezes na frase' .format(frase.count('A')))
+print('a primeira letra A aparece na posição {}' .format(frase.find('A')+1))
+print('a ultima letra A aparece na posição {}'.fomart(frase.rfind('A')))
