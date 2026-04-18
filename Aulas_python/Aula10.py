f676084a7ca5941038e5bb335c2011fb483c8f42
simookoto <simohayha14072011@gmail.com>
2026-04-18 16:47:02 -0300

    update Aulas_python/Aula10.py

---
+++ Aulas_python/Aula10.py
@@ -0,0 +1,7 @@
+tempo=int(input('quantos anos tem seu carro?'))
+print('carro novo'if tempo<=3 else 'carro velho')
+#if tempo<3:
+ # print('carro novo')
+#else:
+ # print('carro velho')#
+print('--fim--')
