f7950982cfce8ce7129bee0d789056b9bda0a260
simookoto <simohayha14072011@gmail.com>
2026-04-18 14:12:05 -0300

    update Exercícios/Exercício26.py

--- Exercícios/Exercício26.py
+++ Exercícios/Exercício26.py
@@ -1,4 +1,4 @@
 frase=str(input('digite uma frase:')).upper()
 print('a letra A aparece {} vezes na frase' .format(frase.count('A')))
 print('a primeira letra A aparece na posição {}' .format(frase.find('A')+1))
-print('a ultima letra A aparece na posição {}'.fomart(frase.rfind('A')))
+print('a ultima letra A aparece na posição {}' .format(frase.rfind('A')+1))
