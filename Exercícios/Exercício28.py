3bb68064ff336c7afbbb97ca85dda44534bedcc3
simookoto <simohayha14072011@gmail.com>
2026-04-20 15:32:10 -0300

    update Exercícios/Exercício28.py

--- Exercícios/Exercício28.py
+++ Exercícios/Exercício28.py
@@ -1,10 +1,13 @@
 from random import randint
+from time import sleep
 computador = randint(0, 5)
 print('=='*11)
 print('eu te desafio a me ganhar!')
 print('vou pensar em um número...')
 print('=='*11)
 jogador=int(input('em que número eu pensei?'))
+print('pensando...')
+sleep(2)
 if jogador == computador:
   print('você ganhou parabens! eu perdi')
 else:
