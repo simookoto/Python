f28a9b757f6b2fb397f29276b7488056ae2a5667
simookoto <simohayha14072011@gmail.com>
2026-04-29 19:40:08 -0300

    update Exercícios/Exercício34.py

--- Exercícios/Exercício34.py
+++ Exercícios/Exercício34.py
@@ -1,4 +1,7 @@
 salario=float(input('qual o salario atual do funcionario? R$'))
 if salario<=1250:
   aumento1=salario+(salario*15/100)
-  print('o salario passara a ser de {}' .format())
+  print('o salario passara a ser de {:.2f}' .format(aumento1))
+else:
+  aumento2=salario+(salario*10/100)
+  print('o salario passara a ser {:.2f}' .format(aumento2))
