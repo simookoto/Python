bd9013abb5c9e6904352ec657d0ef8f286a63190
simookoto <simohayha14072011@gmail.com>
2026-05-04 07:50:42 -0300

    update Exercícios/Exercício35.py

--- Exercícios/Exercício35.py
+++ Exercícios/Exercício35.py
@@ -5,6 +5,6 @@ r1=float(input('primeiro segmento:'))
 r2=float(input('segundo segmento:'))
 r3=float(input('terceiro segmento:'))
 if r1 < r2+r3 and r2 < r1+r3 and r3 < r2+r1:
-  print('os segmentos assima podem ser um triangulo')
+  print('os segmentos acima podem ser um triangulo')
 else:
   print('os segmentos acima não podem formar um triangulo')
