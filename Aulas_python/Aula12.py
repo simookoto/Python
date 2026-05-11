b2b1da8170c779f348f73d0b7b601ab7fe027d04
simookoto <simohayha14072011@gmail.com>
2026-05-11 18:53:01 -0300

    update Aulas_python/Aula12.py

--- Aulas_python/Aula12.py
+++ Aulas_python/Aula12.py
@@ -3,6 +3,8 @@ if nome== 'Daniel':
   print('que nome lindo você tem!')
 elif nome== 'MARIA' or nome== 'JOÃO' or nome== 'JOSÉ':
   print('seu nome é bem popular no brasil')
+elif nome== 'ANA' or nome== 'TADEU':
+  print('espera, você não é um dos pais do Daniel?')
 else:
   print('seu nome é bem normal /:')
 print('tenha um bom dia, {}' .format(nome))
