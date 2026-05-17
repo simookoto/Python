2b9b9e1177a0c86707e318749d2d1d3247c89042
simookoto <simohayha14072011@gmail.com>
2026-05-16 22:10:29 -0300

    update Exercícios/Exercício38.py

---
+++ Exercícios/Exercício38.py
@@ -0,0 +1,8 @@
+num1=int(input('digite um número inteiro: '))
+num2=int(input('digite outro número inteiro: '))
+if num1>num2:
+  print('o primeiro é maior')
+elif num2>num1:
+  print('o segundo é maior')
+else:
+  print('os dois são iguais, não a um maior')
