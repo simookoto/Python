4cb514dcb739f8d5d0d6ebbef5256a3d46e0775a
simookoto <simohayha14072011@gmail.com>
2026-05-16 22:42:12 -0300

    update Exercícios/Exercício39.py

---
+++ Exercícios/Exercício39.py
@@ -0,0 +1,16 @@
+from datetime import date
+atual= date.today().year
+nasc=int(input('em qual ano você nasceu? '))
+idade= atual - nasc
+print('quem nasceu em {}, tem {} anos em {}' .format(nasc, idade, atual))
+if idade == 18:
+  print('você ja deve se alistar no exercito,') 
+  print('esse ano')
+elif idade < 18:
+  saldo= 18 - idade
+  print(' você não tem 18 anos,')
+  print('falta {} anos pra você se alistar' .format(saldo))
+elif idade > 18:
+  saldo= idade - 18
+  print('você tem mais de 18 anos, você')
+  print('devia ter se alistado há {} anos' .format(saldo))
