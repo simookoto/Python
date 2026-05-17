5b507f190238e05c58cb8a1bc8fd9a4a523c1ac8
simookoto <simohayha14072011@gmail.com>
2026-05-16 22:49:24 -0300

    update Exercícios/Exercício39.py

--- Exercícios/Exercício39.py
+++ Exercícios/Exercício39.py
@@ -8,9 +8,13 @@ if idade == 18:
   print('esse ano')
 elif idade < 18:
   saldo= 18 - idade
+  ano= atual+saldo
   print(' você não tem 18 anos,')
   print('falta {} anos pra você se alistar' .format(saldo))
+  print('você deve se alistar em {}' .format(ano))
 elif idade > 18:
   saldo= idade - 18
+  ano= atual-saldo
   print('você tem mais de 18 anos, você')
   print('devia ter se alistado há {} anos' .format(saldo))
+  print('você devia ter se alistado em {}' .format(ano))
