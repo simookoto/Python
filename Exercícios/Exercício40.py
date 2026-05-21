108fdbf52b6162958f568a0a4235535eb9036b11
simookoto <simohayha14072011@gmail.com>
2026-05-21 17:51:26 -0300

    update Exercícios/Exercício40.py

--- Exercícios/Exercício40.py
+++ Exercícios/Exercício40.py
@@ -1,3 +1,9 @@
 not1=float(input('digite sua primeira nota:'))
 not2=float(input('digite sua segunda nota:'))
 media= (not1 + not2) / 2
+if media => 5:
+  print('sua média é {}, você está de recuperação' .format(media))
+elif media =< 6:
+  print('sua média é {}, você está aprovado' .format(media))
+elif media => 3:
+  print('sua média é {}, você está reprovado' .format(media))
