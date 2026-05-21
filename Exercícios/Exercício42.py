f49e4cd011f1abe36f2c1983a017ad069d8271f2
simookoto <simohayha14072011@gmail.com>
2026-05-21 19:01:22 -0300

    update Exercícios/Exercício42.py

---
+++ Exercícios/Exercício42.py
@@ -0,0 +1,17 @@
+print('-=-' *10)
+print('de as areas do triangulo:')
+print('-=-' *10)
+r1=float(input('primeiro segmento: '))
+r2=float(input('segundo segmento: '))
+r3=float(input('terceiro segmento: '))
+if r1 < r2+r3 and r2 < r1+r3 and r3 < r1+r2:
+  print('pode forma um triangulo')
+  if r1 == r2 == r3:
+    ('o triangulo é equilátero!')
+  elif r1 != r2 != r3 != r1:
+      print('o triangulo é escaleno!')
+  else:
+    print('o triangulo é isóceles!')
+else:
+  print('não podem formar um triangulo')
+  
