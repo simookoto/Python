49f837dc41d997134037e9aebec5bd4164a14e04
simookoto <simohayha14072011@gmail.com>
2026-05-27 18:00:05 -0300

    update Exercícios/Exercício44.py

---
+++ Exercícios/Exercício44.py
@@ -0,0 +1,11 @@
+print('====================')
+print('           lojas okoto')
+print('====================')
+compra=int(input('qual o valor da sua compra? R$'))
+print('[1] a vista')
+print('[2] cartão x1')
+print('[3] cartão x2')
+print('[4] cartão x3 ou mais')
+pag=input('qual a sua forma de pagamento? ')
+if pag == 1:
+  desc= compra
