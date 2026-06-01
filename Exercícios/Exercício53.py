9b97f5501c5a7edd1dc44b0854db66c68512ddd0
simookoto <simohayha14072011@gmail.com>
2026-06-01 20:35:13 -0300

    update Exercícios/Exercício53.py

--- Exercícios/Exercício53.py
+++ Exercícios/Exercício53.py
@@ -1,2 +1,10 @@
 frase=str(input('digite uma frase: ')).strip().upper()
-print('você digitou a frase {}' .format(frase))
+palavras=frase.split()
+junto=''.join(palavras)
+inverso= ''
+for letra in range(len(junto)-1, -1, -1):
+  inverso += junto[letra]
+if inverso == junto:
+  print('Temos um palíndromo')
+else:
+  print('A frase digitada não é um palíndromo')
