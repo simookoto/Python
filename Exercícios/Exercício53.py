9e23fcd7b9e229159ec63455d20346236fc18db2
simookoto <simohayha14072011@gmail.com>
2026-06-01 20:45:03 -0300

    update Exercícios/Exercício53.py

--- Exercícios/Exercício53.py
+++ Exercícios/Exercício53.py
@@ -1,9 +1,10 @@
 frase=str(input('digite uma frase: ')).strip().upper()
 palavras=frase.split()
 junto=''.join(palavras)
-inverso= ''
-for letra in range(len(junto)-1, -1, -1):
-  inverso += junto[letra]
+#inverso= ''#
+inverso= junto[::-1]
+#for letra in range(len(junto)-1, -1, -1):#
+ # inverso += junto[letra]#
 print('o inverso de {} é {}' .format(junto, inverso))
 if inverso == junto:
   print('Temos um palíndromo!')
