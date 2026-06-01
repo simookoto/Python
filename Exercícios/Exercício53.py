d9f77d505b540bb5d1d6232fe0955985d7b0b779
simookoto <simohayha14072011@gmail.com>
2026-06-01 20:40:04 -0300

    update Exercícios/Exercício53.py

--- Exercícios/Exercício53.py
+++ Exercícios/Exercício53.py
@@ -4,7 +4,8 @@ junto=''.join(palavras)
 inverso= ''
 for letra in range(len(junto)-1, -1, -1):
   inverso += junto[letra]
+print('o inverso de {} é {}' .format(junto, inverso))
 if inverso == junto:
-  print('Temos um palíndromo')
+  print('Temos um palíndromo!')
 else:
   print('A frase digitada não é um palíndromo')
