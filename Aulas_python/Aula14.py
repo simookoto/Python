86d9c621d7e4dd3480e4520faf2190636b05e7ba
simookoto <simohayha14072011@gmail.com>
2026-06-05 20:25:38 -0300

    update Aulas_python/Aula14.py

--- Aulas_python/Aula14.py
+++ Aulas_python/Aula14.py
@@ -11,8 +11,21 @@ print('FIM!')'''
 '''for c in range(1, 5):
   n=int(input('digite um valor: '))'''
 
-r = 'S'
+'''r = 'S'
 while r == 'S':
   n=int(input('digite um valor: '))
   r=str(input('quer continuar? [S/N] ')).upper()
-print('FIM!')
+print('FIM!')'''
+n= 1
+par= impar= 0
+print('==' *8)
+print('digite 0 para parar')
+print('==' *8)
+while n != 0:
+  n=int(input('digite um valor: '))
+  if n != 0:
+    if n % 2 == 0:
+      par+= 1
+    else:
+      impar+= 1
+print('você digitou {} pares, \n e {} impares' .format(par, impar))
