f2fde48c5a515ade386cec243a1ad9c4177cd6a2
simookoto <simohayha14072011@gmail.com>
2026-06-05 20:07:06 -0300

    update Aulas_python/Aula14.py

---
+++ Aulas_python/Aula14.py
@@ -0,0 +1,18 @@
+'''for c in range(1, 10):
+  print(c)
+print('FIM!')'''
+
+'''c= 1
+while c < 10:
+  print(c)
+  c+=1
+print('FIM!')'''
+
+'''for c in range(1, 5):
+  n=int(input('digite um valor: '))'''
+
+r= 'S'
+while r == 'S':
+  n=int(input('digite um valor: '))
+  r=int(input('quer continuar? [S/N] ')).upper()
+print('FIM!')
