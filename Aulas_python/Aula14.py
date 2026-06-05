2587fd597366eea671e0c00b5a18cb13bf19b2ca
simookoto <simohayha14072011@gmail.com>
2026-06-05 20:12:54 -0300

    update Aulas_python/Aula14.py

--- Aulas_python/Aula14.py
+++ Aulas_python/Aula14.py
@@ -11,8 +11,8 @@ print('FIM!')'''
 '''for c in range(1, 5):
   n=int(input('digite um valor: '))'''
 
-r= 'S'
+r = 'S'
 while r == 'S':
   n=int(input('digite um valor: '))
-  r=int(input('quer continuar? [S/N] ')).upper()
+  r=str(input('quer continuar? [S/N] ')).upper()
 print('FIM!')
