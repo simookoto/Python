c16bd29f12a62a944d5aa47c57203b1718b451ad
simookoto <simohayha14072011@gmail.com>
2026-06-07 20:10:47 -0300

    update Exercícios/Exercício58.py

---
+++ Exercícios/Exercício58.py
@@ -0,0 +1,6 @@
+from random import randint
+computador= randint(1, 10)
+print('=='*13)
+print('olá sou seu celular, eu tenho \n um jogo para você, eu vou \n pensar em um número \n de 1 a 10, e você tenta adivinhar')
+print('=='*13)
+print(' ')
