771002e48ccb9fa821c5a78354adafdbd0780dad
simookoto <simohayha14072011@gmail.com>
2026-06-07 20:34:08 -0300

    update Exercícios/Exercício58.py

--- Exercícios/Exercício58.py
+++ Exercícios/Exercício58.py
@@ -4,3 +4,17 @@ print('=='*13)
 print('olá sou seu celular, eu tenho \n um jogo para você, eu vou \n pensar em um número \n de 1 a 10, e você tenta adivinhar')
 print('=='*13)
 print(' ')
+acertou= False
+palpites= 0
+while not acertou:
+  jogador=int(input('qual é o seu palpite? '))
+  palpites += 1
+  if jogador == computador:
+    acertou= True
+  else:
+    if jogador < computador:
+      print('maior... tente mais uma vez.')
+    elif jogador > computador:
+      print('menor... tente mais uma vez.')
+print('Acertou!')
+print('você precisou de {} tentativa(s), \n parabéns!' .format(palpites))
