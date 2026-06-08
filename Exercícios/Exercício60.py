e8d5011f726d259f5872bfbfabdf6714ba9d02db
simookoto <simohayha14072011@gmail.com>
2026-06-07 21:55:09 -0300

    update Exercícios/Exercício60.py

--- Exercícios/Exercício60.py
+++ Exercícios/Exercício60.py
@@ -1,7 +1,11 @@
 n=int(input('digite um número \npara ver seu fatorial: '))
 c = n
+f = 1
+print('calculando {}! = ' .format(n), end='')
 while c > 0:
   print('{} ' .format(c), end='')
   print(' x ' if c > 1 else ' = ', end='')
+  print(' ', end='')
+  f *= c
   c-= 1
-  
+print('{}' .format(f))
