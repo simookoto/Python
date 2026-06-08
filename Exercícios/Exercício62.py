6842b59b12da23c269a9f6050bfcd8520db948c9
simookoto <simohayha14072011@gmail.com>
2026-06-08 20:52:24 -0300

    update Exercícios/Exercício62.py

--- Exercícios/Exercício62.py
+++ Exercícios/Exercício62.py
@@ -5,12 +5,16 @@ pt=int(input('primeiro termo: '))
 rz=int(input('razão: '))
 termo= pt
 cont= 1
-while cont <= 10:
-  print('{} ' .format(termo), end='')
-  print('→', end='')
-  print(' ', end='')
-  termo+= rz
-  cont+= 1
-  if pt != 0:
-    
+total= 0
+mais= 10
+while mais != 0:
+  total= total + mais
+  while cont <= total:
+    print('{} ' .format(termo), end='')
+    print('→', end='')
+    print(' ', end='')
+    termo+= rz
+    cont+= 1
+  print('PAUSA')
+  mais=int(input('quantos termos você quer \nmostrar a mais? '))
 print('FIM!')
