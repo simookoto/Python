98e4ad7e8560a6d4fee375c7b238104df3f7ac9b
simookoto <simohayha14072011@gmail.com>
2026-06-08 20:42:27 -0300

    update Exercícios/Exercício62.py

--- Exercícios/Exercício62.py
+++ Exercícios/Exercício62.py
@@ -1,3 +1,16 @@
 print('=='*8)
 print('   gerador de P.A.')
 print('=='*8)
+pt=int(input('primeiro termo: '))
+rz=int(input('razão: '))
+termo= pt
+cont= 1
+while cont <= 10:
+  print('{} ' .format(termo), end='')
+  print('→', end='')
+  print(' ', end='')
+  termo+= rz
+  cont+= 1
+  if pt != 0:
+    
+print('FIM!')
