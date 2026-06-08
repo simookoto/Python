1c14334fa27dc76e505876c45b9febf0deb67f43
simookoto <simohayha14072011@gmail.com>
2026-06-07 21:04:43 -0300

    update Exercícios/Exercício59.py

---
+++ Exercícios/Exercício59.py
@@ -0,0 +1,29 @@
+n1=int(input('primeiro valor: '))
+n2=int(input('segundo valor: '))
+opçao= 0
+while opçao != 5:
+  print('''  [1] somar
+  [2] multiplicar
+  [3] maior
+  [4] novos números
+  [5] sair''')
+  opçao=int(input('>>>qual a sua escolha?<<< '))
+  if opçao== 1:
+    soma= n1 + n2
+    print('a soma entre {} + {} = {}' .format(n1, n2, soma))
+  elif opçao== 2:
+    multi= n1 * n2
+    print('a multiplicação entre {} x {} = {}' .format(n1, n2, multi))
+  elif opçao== 3:
+    if n1 < n2:
+      print('o maior número é {}' .format(n2))
+    elif n2 < n1:
+      print('o maior é {}' .format(n1))
+  elif opçao== 4:
+    print('digite denovo os valores')
+  elif opçao== 5:
+    
+  else:
+    print('opção inválida, tente novamente')
+  print('-='*10)
+print('fim do programa, volte sempre!')
