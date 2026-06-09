2d1b7ae9abf6bfe117ba82db20ea28400d091141
simookoto <simohayha14072011@gmail.com>
2026-06-09 20:42:43 -0300

    update Exercícios/Exercício65.py

--- Exercícios/Exercício65.py
+++ Exercícios/Exercício65.py
@@ -1,9 +1,16 @@
 resp= 'S'
-soma= quant= média= 0
+soma= quant= média= maior= menor= 0
 while resp in 'Ss':
   n=int(input('digite um número: '))
   soma+= n
   quant+= 1
+  if quant== 1:
+    maior= menor= n
+  else:
+    if n > maior:
+      maior= n
+    if n < menor:
+      menor= n
   resp= str(input('quer continuar? [S/N]'))
 média= soma/quant
-print('você digitou {} números, \ne a média foi {}' .format(quant, média))
+print('você digitou {} números, \ne a média foi {:.1f}, \no maior valor foi {}, \ne o menor valor foi {}' .format(quant, média, maior, menor))
