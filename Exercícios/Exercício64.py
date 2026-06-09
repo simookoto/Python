f3711677c16e1840b3284721c9d61dd47c207063
simookoto <simohayha14072011@gmail.com>
2026-06-09 20:08:33 -0300

    update Exercícios/Exercício64.py

--- Exercícios/Exercício64.py
+++ Exercícios/Exercício64.py
@@ -2,3 +2,5 @@ n= int(input('digite um número [999 para parar]: '))
 cont= 0
 while n != 999:
   n= int(input('digite um número [999 para parar]: '))
+  cont+= 1
+print('a soma dos números da {}' .format())
