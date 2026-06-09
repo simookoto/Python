c14afde7fc586ab2c5d84c966dc822cbf3785548
simookoto <simohayha14072011@gmail.com>
2026-06-09 07:49:06 -0300

    update Teste_monkeys.py/Teate01.py

---
+++ Teste_monkeys.py/Teate01.py
@@ -0,0 +1,13 @@
+print('-=-'*5)
+print('recaptcha')
+print('-=-'*5)
+print('captcha:')
+print('[1] para robô')
+print('[2] humano')
+print(' ')
+robo=int(input(''))
+if robo== 1:
+  print('robô vagabundo')
+elif robo== 2:
+  teste=int(input('qual a raiz quadrada de 128? '))
+  if 
