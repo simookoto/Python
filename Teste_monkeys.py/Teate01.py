218028e2a8b4091b41bcff020530cf52a0e565f1
simookoto <simohayha14072011@gmail.com>
2026-06-09 07:54:20 -0300

    update Teste_monkeys.py/Teate01.py

--- Teste_monkeys.py/Teate01.py
+++ Teste_monkeys.py/Teate01.py
@@ -9,5 +9,10 @@ robo=int(input(''))
 if robo== 1:
   print('robô vagabundo')
 elif robo== 2:
-  teste=int(input('qual a raiz quadrada de 128? '))
-  if 
+  teste=int(input('qual a raiz quadrada de 144? '))
+  if teste== 12:
+    print('você é um robô! adeus')
+  elif teste != 12:
+    print('você é um humano')
+else:
+  print('error 404!')
