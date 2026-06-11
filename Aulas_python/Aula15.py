40a885afa80963295f8f520ce28a1c3e01b1ae12
simookoto <eu.danielfreitasdeoliveira1@gmail.com>
2026-06-11 18:38:47 -0300

    atualiza essa porra

---
+++ Aulas_python/Aula15.py
@@ -0,0 +1,17 @@
+'''cont= 1
+while True:
+  print(cont, ' ', end='')
+  cont+= 1
+print('ACABOU')'''
+'''n= s= 0
+while True:
+  n= int(input('digite um número: '))
+  if n == 999:
+    break
+  s+= n
+#print('a soma vale {}' .format(s))
+print(f'a soma vale {s}')'''
+nome= 'Daniel'
+idade= 15
+salario= 4000
+print(f'o {nome} tem {idade} \ne recebe R${salario:.2f}')
--- Exercícios/Exercício21.py
+++ Exercícios/Exercício21.py
@@ -1,5 +1,18 @@
-import pygame
+"""
+import pygame;
 pygame.init()
 pygame.mixer.music.load('anirapwb.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
+"""
+
+import pygame
+
+pygame.init()
+pygame.mixer.music.load('anirapwb.mp3')
+pygame.mixer.music.play()
+
+# Mantém o programa rodando enquanto a música estiver tocando
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)  # Evita que o processo consuma 100% da sua CPU
+
