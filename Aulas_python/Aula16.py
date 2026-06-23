40e9fb3dafe6a3341dd434864a76420b8347bf9a
simookoto <eu.danielfreitasdeoliveira1@gmail.com>
2026-06-23 18:54:46 -0300

    atualize

---
+++ Aulas_python/Aula16.py
@@ -0,0 +1,8 @@
+lanche= ('hamburguer', 'monster', 'pizza', 'pudim')
+#tlupas são imudaveis
+'''print(lanche[2])
+print(lanche[-3])
+print(lanche[1:3])'''
+for comida in lanche:
+  print(f'eu vou comer {comida}')
+print('estou satisfeito!')
