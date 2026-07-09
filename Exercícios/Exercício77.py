61388e0c152d1282507804c2fc4399d6ff1995e0
simookoto <eu.danielfreitasdeoliveira1@gmail.com>
2026-07-09 07:47:49 -0300

    atualize

--- Exercícios/Exercício77.py
+++ Exercícios/Exercício77.py
@@ -1,6 +1,6 @@
 palavras= ('caderno', 'nome', 'pessoa', 'garagem', 'computador', 'engrenagem')
 for pala in palavras:
-  print(f'\nna palavra {p} temos ', end='')
+  print(f'\nna palavra {pala} temos ', end='')
   for letras in pala:
     if letras.lower() in 'aeiou':
       print( letras, end=' ')
