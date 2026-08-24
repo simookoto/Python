4a80fcf6c12e9e4f8b6715758b3e016b26f45162
simookoto <eu.danielfreitasdeoliveira1@gmail.com>
2026-08-24 19:24:56 -0300

    atualize

---
+++ Exercícios/Exercício86.py
@@ -0,0 +1,10 @@
+matriz= [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for lin in range(0, 3):
+ for col in range(0, 3):
+  matriz[lin][col]= int(input(f'digite um valor para [{lin}, {col}]: '))
+print('=='*15)
+for lin in range(0, 3):
+  for col in range(0, 3):
+    print(f'[{matriz[lin][col]:^5}]', end='')
+print('', end='')
+print()
